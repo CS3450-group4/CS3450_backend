92663ce47f7a44d30bb0cf6e88a4012c89884224
Jacob Thompson <a01657923@usu.edu>
2022-09-11 23:05:28 -0600

    Setup django api

---
+++ api/views.py
@@ -0,0 +1,18 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import MenuItem
+from .serializers import MenuItemSerializer
+
+
+@api_view(['GET'])
+def getProducts(request):
+    menuItems = MenuItem.objects.all()
+    serializer = MenuItemSerializer(menuItems, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addMenuItem(request):
+    serializer = MenuItemSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response(serializer.data)
