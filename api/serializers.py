701062fd48de57ddfc278ad368ae8a37014d4479
Jacob Thompson <a01657923@usu.edu>
2022-10-29 20:59:00 -0600

    custom updater for nested data

--- api/serializers.py
+++ api/serializers.py
@@ -26,6 +26,30 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = ["id", "username", "first_name", "last_name", "orders", "userinfo"]
 
+    def update(self, instance, validated_data):
+        if validated_data.get("userinfo"):
+            user_info_data = validated_data.pop("userinfo")
+            userInfo = instance.userinfo
+            if user_info_data.get("authLevel"):
+                userInfo.authLevel = user_info_data.get("authLevel")
+
+            if user_info_data.get("balance"):
+                userInfo.balance = user_info_data.get("balance")
+
+            if user_info_data.get("hoursWorked"):
+                userInfo.hoursWorked = user_info_data.get("hoursWorked")
+            userInfo.save()
+
+        if validated_data.get("username"):
+            instance.username = validated_data.get("username")
+        if validated_data.get("first_name"):
+            instance.first_name = validated_data.get("first_name")
+        if validated_data.get("last_name"):
+            instance.last_name = validated_data.get("last_name")
+
+        instance.save()
+        return instance
+
 
 class IngredientSerializer(serializers.ModelSerializer):
     class Meta:
